0a497a1cb096445414bae3fb66698cb5d01143c9
ahkubek <120653318+ahkubek@users.noreply.github.com>
2024-01-25 16:32:09 +0300

    tochka

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+    import java.util.*;
+    public class Main
+    {
+        public static void main(String[] args)throws java.io.IOException {
+            char ch;
+            //Scanner sc = new Scanner(System.in);
+            ch=(char) System.in.read();
+            System.out.println("вы ввели "+ch);
+
+
+
+
+        }
+    }
